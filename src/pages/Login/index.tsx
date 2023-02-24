2da7727b89488d7e4d0f1c67a91a3933d30de19a
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-02-23 22:42:10 -0300

    Refactor: Improve login page:

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,17 +1,51 @@
 import { BsGoogle } from "react-icons/bs"
 import { MdLogin } from "react-icons/md";
+
 import LoginImg from "../../assets/login.jpg"
+
 import { Header } from '../../components/Header/index';
 import { CustomButton } from "../../components/CustomButton";
-import { LoginContainer, LoginContent, LoginContentForm, LoginHeadline, LoginSubtitle } from "./login"
+
+import { InputContainer, LoginContainer, LoginContent, LoginContentForm, LoginHeadline, LoginInputContainer, LoginSubtitle, RegisterButton } from "./login"
+
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+import { Link } from 'react-router-dom';
+import { ErrorMessage } from "../../components/ErrorMessage";
+import { ImageContainer } from "../../components/ImgeContainer";
+
+interface ISignIn {
+  email: string;
+  password: string;
+}
 
 export const Login = () => {
+
+  const schema = yup.object({
+    email: yup.string()
+      .email("Digite um email com formato válido")
+      .required('Email é um campo obrigatório'),
+    password: yup.string()
+      .required('Senha é um campo obrigatório'),
+  })
+
+  const { register, handleSubmit, formState: { errors } } = useForm<ISignIn>({ resolver: yupResolver(schema) });
+
+  const handleSubmitClick = (data: any) => {
+    console.log({ data })
+  }
+
+
   return (
     <>
       <Header />
       <LoginContainer>
         <LoginContent>
-          <img src={LoginImg} alt="" />
+
+          <ImageContainer src={LoginImg} alt="Homem olhando roupas" />
+          {/* <img src={LoginImg} alt="" /> */}
+
           <LoginContentForm>
             <LoginHeadline>Entre com a sua conta</LoginHeadline>
             <CustomButton startIcon={<BsGoogle size={18} />}>
@@ -19,9 +53,34 @@ export const Login = () => {
               Entrar com o Google
             </CustomButton>
             <LoginSubtitle>ou entre com o seu e-mail</LoginSubtitle>
-            <CustomButton startIcon={<MdLogin size={18} />}>
+
+            <LoginInputContainer>
+              <p>E-mail</p>
+              <InputContainer hasError={!!errors?.email} type="text" placeholder="Digite seu e-mail" {...register('email')} required />
+              <ErrorMessage>{errors.email?.message}</ErrorMessage>
+
+            </LoginInputContainer>
+
+            <LoginInputContainer>
+              <p>Senha</p>
+              <InputContainer hasError={!!errors?.password} placeholder="Digite sua senha" {...register('password')} type="password" />
+              <ErrorMessage>{errors.password?.message}</ErrorMessage>
+
+            </LoginInputContainer>
+
+            <CustomButton
+              startIcon={<MdLogin size={20} />}
+              onClick={() => handleSubmit(handleSubmitClick)()}>
+
               Entrar
             </CustomButton>
+
+            <p>Ainda não tem uma conta?
+
+              <Link to='/cadastro'>
+                <RegisterButton>Cadastre-se</RegisterButton>
+              </Link>
+            </p>
 
           </LoginContentForm>
         </LoginContent >
--- src/pages/Login/login.ts
+++ src/pages/Login/login.ts
@@ -1,5 +1,10 @@
 import styled from 'styled-components';
 
+type CustomInputContainerProps = {
+  hasError?: boolean
+}
+
+
 export const LoginContainer = styled.div`
   height: 100%;
   width: 100%;
@@ -14,14 +19,8 @@ export const LoginContent = styled.div`
   align-items: center;
   flex-grow: 1;
   margin: 10px 0;
-  gap: 100px;
-  
-  img {
-    width: 50%;
-    height: 90%;
-    object-fit: cover;
-    border-radius: 20px;
-  }
+  gap: 50px;
+
 `;
 
 export const LoginHeadline = styled.p`
@@ -34,9 +33,13 @@ export const LoginHeadline = styled.p`
 export const LoginSubtitle = styled.p`
   color: var(--dark-500);
   padding-bottom: 1.25rem;
+
   border-bottom: 1px solid #6c757d;
+
   width: 100%;
+
   margin-top: 1.25rem;
+
   text-align: center;
   font-weight: 500;
   margin-bottom: 1.25rem;
@@ -45,8 +48,49 @@ export const LoginSubtitle = styled.p`
 
 export const LoginContentForm = styled.div`
   width: 50%;
+
    display: flex; 
    flex-direction: column;
    align-items: center;
    justify-content: center;
+
+   gap: 10px;
+
+`;
+
+export const LoginInputContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 1.25rem;
+`;
+
+export const InputContainer = styled.input<CustomInputContainerProps>`
+  border: none;
+
+  width: 100%;
+  background-color: var(--input);
+
+  padding: 1rem;
+
+  border-radius: 10px;
+
+  color: var(--black-500);
+  border: ${(props) => (props.hasError ? `2px solid var(--error)` : 'none')};
+
+  &::placeholder {
+    color: ${(props) =>
+    props.hasError ? 'var(--error)' : 'var(--placeholder)'};
+  }
+
+  &:focus {
+      outline: ${(props) =>
+    props.hasError ? 'none' : `2px solid var(--placeholder)`}
+  }
+`;
+
+
+export const RegisterButton = styled.span`
+  font-weight: 600;
+  margin-left: 8px;
 `;
