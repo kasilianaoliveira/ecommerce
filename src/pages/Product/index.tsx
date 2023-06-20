a641c52bf47eb85219b0192233ee51039ba70293
kasilianaoliveira <kasilianaoliveira@gmail.com>
2023-06-20 10:28:22 -0300

    Feat: add filter

--- src/pages/Product/index.tsx
+++ src/pages/Product/index.tsx
@@ -1,24 +1,51 @@
-import { useContext, useEffect } from 'react';
+import { FormEvent, useContext, useEffect, useState } from 'react';
 import { Header } from '../../components/Header'
 import { ProductCategory } from '../../components/ProductCategory'
 import { CategoriesContext } from '../../context/categoriesContext';
 import { useParams } from 'react-router-dom';
-import { ProductContainer, ProductContent } from './product';
+import { ContentFilter, ProductContainer, ProductContent, ProductFilter } from './product';
 
 export const Product = () => {
   const { categories, fetchCategories, isLoadingCategories } = useContext(CategoriesContext);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [priceRange, setPriceRange] = useState('');
+
+  const handleInputChange = (event: any) => {
+    setSearchTerm(event.target.value);
+  };
+
+
+  const handlePriceRangeChange = (event: any) => {
+    setPriceRange(event.target.value);
+  };
 
   const { nameProduct } = useParams();
 
   useEffect(() => {
     fetchCategories()
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
+
+
 
   const filteredProducts = categories
     .filter(category => category.displayName === nameProduct)
     .map(category => category.products)
     .flat()
+    .filter(product => product.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    .filter(product => {
+      if (priceRange === '') {
+        return true; // Se nenhum intervalo de preço for selecionado, todos os produtos são exibidos
+      }
+      if (priceRange === 'low') {
+        return product.price >= 0 && product.price <= 200;
+      } else if (priceRange === 'medium') {
+        return product.price > 200 && product.price <= 499;
+      } else if (priceRange === 'high') {
+        return product.price >= 500;
+      }
+    });
+
 
 
 
@@ -26,14 +53,32 @@ export const Product = () => {
     <ProductContainer>
       <Header />
       {/* <h1>Produtos</h1> */}
-      <ProductContent>
-        {filteredProducts.map(product => (
-          <ProductCategory
-            key={product.id}
-            product={product}
+      <ProductFilter>
+
+        <ContentFilter>
+          <input
+            type="text"
+            placeholder="Digite nome do produto"
+            value={searchTerm}
+            onChange={handleInputChange}
           />
-        ))}
-      </ProductContent>
+          <select value={priceRange} onChange={handlePriceRangeChange}>
+            <option value="">Todos os preços</option>
+            <option value="low">Até R$ 200</option>
+            <option value="medium">De R$ 200 a R$ 400</option>
+            <option value="high">Acima de R$ 500</option>
+          </select>
+
+        </ContentFilter>
+        <ProductContent>
+          {filteredProducts.map(product => (
+            <ProductCategory
+              key={product.id}
+              product={product}
+            />
+          ))}
+        </ProductContent>
+      </ProductFilter>
     </ProductContainer>
   )
 }
--- src/pages/Product/product.ts
+++ src/pages/Product/product.ts
@@ -4,10 +4,53 @@ export const ProductContainer = styled.div`
 
 `;
 
+export const ProductFilter = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 50px 0;
+
+  input {
+    width: 31.25rem;
+    border-radius: 10px;
+    padding: 15px;
+    background:#e2e2e2;
+    color: black;
+    outline: 0;
+
+    ::placeholder{
+      color: #b4b4b4;
+    }
+  }
+`;
+export const ContentFilter = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  justify-content: space-between;
+
+
+  select {
+    outline: 0;
+    border-radius: 10px;
+
+
+    option{
+    border-radius: 10px;
+
+      
+    :hover {
+      background-color: #ebedef;
+    }
+   }
+  }
+
+
+`;
 export const ProductContent = styled.div`
   max-width: 1200px;
   width: 100%;
-  margin: 0 auto;  
+  /* margin: 0 auto;   */
   /* height: 100vh; */
 
   display: flex;
